74bce7bc627e6daa78e23c5f59b862a91d13f2e4
WhaleFall <2734184475@qq.com>
2021-02-20 20:37:25 +0800

    填坑ing

--- gun.py
+++ gun.py
@@ -1,23 +1,15 @@
 '''
 Author: your name
 Date: 2021-02-20 19:22:44
-LastEditTime: 2021-02-20 20:07:22
+LastEditTime: 2021-02-20 20:26:15
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \PythonAPI\gun.py
 '''
 # gunicorn 并不支持windows，只能在linux 上跑
-import os 
-import gevent.monkey
-gevent.monkey.patch_all()
-import multiprocessing
-
-# 服务地址（adderes:port） 
-bind = "0.0.0.0:5000" 
-# 启动进程数量
-workers = multiprocessing.cpu_count() * 2 +1
-worker_class = 'gevent'
-threads = 20
-preload_app = True
-reload = True
-x_forwarded_for_header = 'X_FORWARDED-FOR'
+bind = "0.0.0.0:5000" #绑定的ip端口号 
+backlog = 512 #监听队列 
+timeout = 30 #超时 
+worker_class = "gevent" #使用gevent模式，还可以使用sync 模式，默认的是sync模式 
+workers = 4 #进程数
+threads = 16 #指定每个进程开启的线程数
